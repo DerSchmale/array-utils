82ef1ba689e93bccbab95c274caf5e1a8ed49fe9
David Lenaerts <david.lenaerts@gmail.com>
2022-01-16 21:36:08 +0100

    add explicit type

--- src/count.ts
+++ src/count.ts
@@ -1,7 +1,7 @@
 /**
  * Returns the number of elements in a (sparse) Array.
  */
-export function count<T>(arr: Array<T>)
+export function count<T>(arr: Array<T>): number
 {
     return arr.reduce(x => x+1, 0);
 }
